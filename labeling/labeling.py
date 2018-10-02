a363b7d563bb9450f431a67713c7c28679fa8d11
Stefan <psn237@nyu.edu>
2018-10-02 22:08:06 +0400

    Refactored more and created a work-aroud for the global variables mouse call back problem

--- labeling/labeling.py
+++ labeling/labeling.py
@@ -5,6 +5,7 @@ import csv
 import os
 import re
 import numpy as np
+import cfg
 
 # initialize the list of reference points and boolean indicating
 # whether cropping is being performed or not
@@ -15,9 +16,14 @@ parser.add_argument("folder", help="Folder name with images to make labels for")
 parser.add_argument("-o", "--output", default=None, help="Image label csv file name. Default is <folder>.csv")
 args = parser.parse_args()
 
+refPt = cfg.refPt
+cropping = cfg.cropping
+cv_im = cfg.cv_im
+clone = cfg.clone
+
 def click_and_crop(event, x, y, flags, param):
     # grab references to the global variables
-    global refPt, cropping, cv_im
+    global refPt, cropping, cv_im, clone
     drawing = False
 
     # if the left mouse button was clicked, record the starting
@@ -32,7 +38,7 @@ def click_and_crop(event, x, y, flags, param):
             cv_im = clone.copy()
             orig_x = refPt[0][0]
             orig_y = refPt[0][1]
-            cv2.rectangle(cv_im, (orig_x, orig_y), (x, y), (0,255,0), 2)
+            cv2.rectangle(cv_im, (orig_x, orig_y), (x, y), (0,255,0), 1)
             cv2.imshow("image", cv_im)
 
     # check to see if the left mouse button was released
@@ -44,7 +50,7 @@ def click_and_crop(event, x, y, flags, param):
         cropping = False
 
         # draw a rectangle around the region of interest
-        cv2.rectangle(cv_im, refPt[0], refPt[1], (0, 255, 0), 2)
+        cv2.rectangle(cv_im, refPt[0], refPt[1], (0, 255, 0), 1)
         cv2.imshow("image", cv_im)
 
 def auto_bbox(image):
@@ -90,80 +96,89 @@ def add_label(outfile, im_name, prob, top_x, top_y, bot_x, bot_y):
     writer.writerow(entry_list)
     return
 
-images = []
-refPt = []
-cropping = False
+def unprocessed_images(outfile):
+    # Set file names to read from / save into
+    if args.output is not None:
+        outfile = args.output
+    else:
+        outfile = args.folder + ".csv"
 
-# Set file names to read from / save into
-dir = args.folder
-if args.output is not None:
-    outfile = args.output
-else:
-    outfile = args.folder + ".csv"
+    # Check if any entries have been added before
+    existing_entries = set()
+    if os.path.isfile(outfile):
+        file = open(outfile, 'r')
+        reader = csv.reader(file, delimiter=',', quotechar='"')
+        for entry_list in reader:
+            image_path = entry_list[0]
+            existing_entries.add(image_path)
 
-# Check if any entries have been added before
-existing_entries = set()
-if os.path.isfile(outfile):
-    file = open(outfile, 'r')
-    reader = csv.reader(file, delimiter=',', quotechar='"')
-    for entry_list in reader:
-        image_path = entry_list[0]
-        existing_entries.add(image_path)
+    # Populate the list of images to make a list for
+    images = []
+    for file in os.listdir(args.folder):
+        if (file.endswith(".jpg") or file.endswith(".jpeg")) and (args.folder + "/" + file not in existing_entries):
+            images.append(args.folder + "/" + file)
+    return images
 
-# print(existing_entries)
+def main():
+    global refPt, cropping, cv_im, clone
 
-# Populate the list of images to make a list for
-for file in os.listdir(dir):
-    if (file.endswith(".jpg") or file.endswith(".jpeg")) and (dir + "/" + file not in existing_entries):
-        images.append(dir + "/" + file)
+    if args.output is not None:
+        outfile = args.output
+    else:
+        outfile = args.folder + ".csv"
 
-# Check if the entire dataset have been processed before
-if not images:
-    print("All images in the given folder have been labeled. Exiting.. ")
-    exit()
+    unlabeled_images = unprocessed_images(outfile)
+    # Check if the entire dataset have been processed before
+    if not unlabeled_images:
+        print("All images in the given folder have been labeled. Exiting.. ")
+        exit()
+    else:
+        print_help()
 
-print_help()
+    for image in unlabeled_images:
+        cv_im = cv2.imread(image)
+        clone = cv_im.copy()
+        refPt = auto_bbox(cv_im)
+        cv2.rectangle(cv_im, refPt[0], refPt[1], (0, 255, 0), 1)
 
-for image in images:
-    cv_im = cv2.imread(image)
-    clone = cv_im.copy()
-    refPt = auto_bbox(cv_im)
-    cv2.rectangle(cv_im, refPt[0], refPt[1], (0, 255, 0), 2)
+        cv2.namedWindow("image")
+        cv2.moveWindow("image", 20, 20)
+        cv2.setMouseCallback("image", click_and_crop)
 
-    cv2.namedWindow("image")
-    cv2.moveWindow("image", 20, 20)
-    cv2.setMouseCallback("image", click_and_crop)
+        # keep looping until the 'q' key is pressed
+        cycle = True
+        while cycle:
+            # display the image and wait for a keypress
+            cv2.imshow("image", cv_im)
+            key = cv2.waitKey(0) & 0xFF
 
-    # keep looping until the 'q' key is pressed
-    cycle = True
-    while cycle:
-        # display the image and wait for a keypress
-        cv2.imshow("image", cv_im)
-        key = cv2.waitKey(0) & 0xFF
+            # if the 'x' key is pressed, the image is not viable. Delete the image
+            if key == ord("x"):
+                os.remove(image)
+                break
 
-        # if the 'x' key is pressed, the image is not viable. Delete the image
-        if key == ord("x"):
-            os.remove(image)
+            # if the 'd' key is pressed, there is no drone in the image
+            elif key == ord("d"):
+                add_label(outfile, image, "0", 0, 0, 0, 0)
+                break
+
+            # if the 's' key is pressed, save the bbox coordinates and break from the loop
+            elif key == ord("s"):
+                add_label(outfile, image, "1", refPt[0][0], refPt[0][1], refPt[1][0], refPt[1][1])
+                break
+
+            # if the 'q' key is pressed, stop labeling and exit
+            elif key == ord("q"):
+                cycle = False
+
+        if not cycle:
             break
 
-        # if the 'd' key is pressed, there is no drone in the image
-        elif key == ord("d"):
-            add_label(outfile, image, "0", 0, 0, 0, 0)
-            break
+    # close all open windows
+    cv2.destroyAllWindows()
 
-        # if the 's' key is pressed, save the bbox coordinates and break from the loop
-        elif key == ord("s"):
-            add_label(outfile, image, "1", refPt[0][0], refPt[0][1], refPt[1][0], refPt[1][1])
-            break
+    print("Folder completed. Exiting..")
 
-        # if the 'q' key is pressed, stop labeling and exit
-        elif key == ord("q"):
-            cycle = False
 
-    if not cycle:
-        break
-
-# close all open windows
-cv2.destroyAllWindows()
-
-print("Folder completed. Exiting..")
+if __name__ == '__main__':
+    main()
