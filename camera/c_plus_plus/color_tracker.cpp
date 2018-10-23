4e6964bf2f1ec558454760d004296f2e0c3be780
Stefan <psn237@nyu.edu>
2018-10-23 10:32:21 +0400

    C++ command-line parsing with boost

--- camera/c_plus_plus/color_tracker.cpp
+++ camera/c_plus_plus/color_tracker.cpp
@@ -10,14 +10,14 @@ int main(int argc, char** argv )
 {
     // Command-line arguments
     namespace po = boost::program_options;
-    int opt;
     po::options_description desc("Allowed options");
-    desc.add_options()
-    ('H,host', po::value<vector<string>>(&opt)->default_value('192.168.1.40'), "Host address for the camera for PTZ control")
-    ('p,port', po::value<vector<int>>(&opt)->default_value(5678), "Port for TCP comms PTZ control")
-    ('d,dev', po::value<vector<int>>(&opt)->default_value(0), 'Camera USB device location for OpenCV')
-    ('c,count', po::value<vector<int>>(&opt)->default_value(600), 'Number of frames to run for non-GUI mode')
-    ('g,gui', po::value<vector<bool>>(&opt)->default_value(true),'GUI mode');
+    desc.add_options()  // returns special proxy object that defines operator()
+        ("help,h", "produce help message")
+        ("host,H", po::value<string>()->default_value("192.168.1.40"), "Host address for the camera for PTZ control")
+        ("port,p", po::value<int>()->default_value(5678), "Port for TCP comms PTZ control")
+        ("dev,d", po::value<int>()->default_value(0), "Camera USB device location for OpenCV")
+        ("count,c", po::value<int>()->default_value(600), "Number of frames to run for non-GUI mode")
+        ("gui,g", po::value<bool>()->default_value(true),"GUI mode");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -30,5 +30,4 @@ int main(int argc, char** argv )
     if (vm.count("host")) {
         cout << vm["host"].as<string>() << endl;
     }
-
 }
