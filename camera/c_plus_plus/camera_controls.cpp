f69da94b7bf0f1525e8466a569ace279afc4f420
Stefan <psn237@nyu.edu>
2018-10-28 12:15:10 +0400

    Camera Controls for PTZOptics Camera using UDP client

--- camera/c_plus_plus/camera_controls.cpp
+++ camera/c_plus_plus/camera_controls.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include <string>
 #include <exception>
+#include <sstream>
 #include <unistd.h>
 
 
@@ -95,6 +96,62 @@ private:
     bool ptContinuousMotion = false;
     bool zContinuous = false;
 
+    bool move(std::string& comm, const int pan, const int tilt) {
+        std::stringstream stream;
+        stream << std::hex << pan;
+        std::string h1( stream.str() );
+
+        stream.str(std::string());
+        stream << std::hex << tilt;
+        std::string h2( stream.str() );
+
+        if (h1.length() < 2) { h1.insert(0, "0"); }
+        if (h2.length() < 2) { h2.insert(0, "0"); }
+        ptContinuousMotion = true;
+        return command(comm.replace(8, 2, h1).replace(10, 2, h2));
+    }
+
+    void formatCommand(std::string& comm, const int pan,const int tilt, const size_t speed) {
+        std::stringstream stream;
+        stream << std::hex << speed;
+        std::string speed_hex( stream.str() );
+        if (speed_hex.length() < 2) { speed_hex.insert(0, "0"); }
+
+        // Format pan variable
+        stream.str(std::string());
+        stream << std::hex << pan;
+        std::string pan_hex( stream.str() );
+        if (pan_hex.length() <= 3) {
+            std::string zeros(4-pan_hex.length(), '0');
+            pan_hex = zeros + pan_hex;
+        }
+        stream.str(std::string());
+        for (size_t index = 0; index < pan_hex.length(); index++) {
+            stream << "0" << pan_hex[index];
+        }
+        pan_hex = stream.str();
+
+        // Format tilt variable
+        stream.str(std::string());
+        stream << std::hex << tilt;
+        std::string tilt_hex( stream.str() );
+        if (tilt_hex.length() <= 3) {
+            std::string zeros(4-tilt_hex.length(), '0');
+            tilt_hex = zeros + tilt_hex;
+        }
+        stream.str(std::string());
+        for (size_t index = 0; index < tilt_hex.length(); index++) {
+            stream << "0" << tilt_hex[index];
+        }
+        tilt_hex = stream.str();
+
+        comm.replace(
+            8, 2, speed_hex).replace(
+            10, 2, speed_hex).replace(
+            12, 4, pan_hex).replace(
+            16, 4, tilt_hex);
+    }
+
 public:
     PTZOptics20x(const std::string& host, const std::string& port)
         : UDPCamera(host, port) {}
@@ -121,12 +178,12 @@ public:
         :return: tilt position
         :rtype: int
     */
-    std::tuple<unsigned int, unsigned int> getPanTiltPosition() {
+    std::tuple<int, int> getPanTiltPosition() {
         command("81090612FF");
         std::string response = read();
         std::string r;
-        unsigned int pan;
-        unsigned int tilt;
+        int pan;
+        int tilt;
         if (response.length() == 16) {
             for (size_t index = 1; index < 9; index += 2) {
                 r += response[index];
@@ -139,16 +196,245 @@ public:
             tilt = std::stoul(r, nullptr, 16);
             return std::make_tuple(pan, tilt);
         }
-        return std::make_tuple(1, -1);
+        return std::make_tuple(-1, -1);
     }
 
+    /*
+    Moves camera to home position.
+
+        :return: True if successful, False if not.
+        :rtype: bool
+    */
     bool home() {
         ptContinuousMotion = false;
         return command("81010604FF");
     }
 
+    /*
+    Resets camera.
 
+            :return: True if successful, False if not.
+            :rtype: bool
+    */
+    bool reset() {
+        ptContinuousMotion = false;
+        zContinuous = false;
+        return command("81010605FF");
+    }
 
+    /*
+    Stops camera movement (pan/tilt).
+
+        :return: True if successful, False if not.
+        :rtype: bool
+    */
+    bool stop() {
+        ptContinuousMotion = false;
+        return command("8101060115150303FF");
+    }
+
+    /*
+    Cancels current command.
+
+        :return: True if successful, False if not.
+        :rtype: bool
+    */
+    bool cancel() {
+        ptContinuousMotion = false;
+        zContinuous = false;
+        return command("81010001FF");
+    }
+
+    /*
+    Moves camera to absolute pan and tilt coordinates.
+
+        :param speed: Speed (0-24)
+        :param pan: numeric pan position
+        :param tilt: numeric tilt position
+        :return: True if successful, False if not.
+        :rtype: bool
+    */
+    bool goTo (const int pan, const int tilt, const size_t speed=5) {
+        std::string comm = "81010602VVWWYYYYZZZZFF";
+        formatCommand(comm, pan, tilt, speed);
+        ptContinuousMotion = false;
+        return command(comm);
+    }
+
+    /*
+    Moves camera to relative pan and tilt coordinates.
+
+        :param speed: Speed (0-24)
+        :param pan: numeric pan adjustment
+        :param tilt: numeric tilt adjustment
+        :return: True if successful, False if not.
+        :rtype: bool
+    */
+    bool goToIncremental(const int pan, const int tilt, const size_t speed=5) {
+        std::string comm = "81010603VVWWYYYYZZZZFF";
+        formatCommand(comm, pan, tilt, speed);
+        ptContinuousMotion = false;
+        return command(comm);
+    }
+
+    /*
+    Halt the zoom motor
+
+        :return: True on success, False on failure
+        :rtype: bool
+    */
+    bool zoomStop() {
+        std::string comm = "8101040700FF";
+        zContinuous = false;
+        return command(comm);
+    }
+
+    /*
+    Initiate tele zoom at speed range 0-7
+
+        :param speed: zoom speed, 0-7
+        :return: True on success, False on failure
+        :rtype: bool
+    */
+    bool zoomIn(const size_t speed=0) {
+        if (speed < 0 || speed > 7) { return false; }
+
+        std::stringstream stream;
+        stream << speed;
+        std::string speed_hex = stream.str();
+        std::string comm = "810104072pFF";
+        comm.replace(9, 1, speed_hex);
+
+        std::cout << "zoomIn comm string: " << comm << std::endl;
+        zContinuous = true;
+        return command(comm);
+    }
+
+    /*
+    Moves camera to absolute zoom setting.
+
+        :param zoom: numeric zoom position
+        :return: True if successful, False if not.
+        :rtype: bool
+    */
+    bool zoomTo(const int zoom) {
+        std::stringstream stream;
+        stream << std::hex << zoom;
+        std::string zoom_hex( stream.str() );
+        if (zoom_hex.length() <= 3) {
+            std::string zeros(4-zoom_hex.length(), '0');
+            zoom_hex = zeros + zoom_hex;
+        }
+        stream.str(std::string());
+        for (size_t index = 0; index < zoom_hex.length(); index++) {
+            stream << "0" << zoom_hex[index];
+        }
+        zoom_hex = stream.str();
+
+        std::string comm = "81010447pqrsFF";
+        comm.replace(8, 4, zoom_hex);
+        return command(comm);
+    }
+
+    /*
+    Modifies pan speed to left.
+
+        :param amount: Speed (0-24)
+        :return: True if successful, False if not.
+        :rtype: bool
+    */
+    bool left(const size_t amount=5) {
+        std::stringstream stream;
+        stream << std::hex << amount;
+        std::string hex_string = stream.str();
+        if (hex_string.length() < 2) {
+            hex_string = "0" + hex_string;
+        }
+        std::string comm = "81010601VVWW0103FF";
+        comm.replace(8, 2, hex_string).replace(10, 2, "15");
+        ptContinuousMotion = true;
+        return command(comm);
+    }
+
+    /*
+    Modifies pan speed to right
+
+        :param amount: Speed (0-24)
+        :return: True if successful, False if not.
+        :rtype: bool
+    */
+    bool right(const size_t amount=5) {
+        std::stringstream stream;
+        stream << std::hex << amount;
+        std::string hex_string = stream.str();
+        if (hex_string.length() < 2) {
+            hex_string = "0" + hex_string;
+        }
+        std::string comm = "81010601VVWW0203FF";
+        comm.replace(8, 2, hex_string).replace(10, 2, "15");
+        ptContinuousMotion = true;
+        return command(comm);
+    }
+
+    /*
+    Modifies pan speed to up.
+
+        :param amount: Speed (0-24)
+        :return: True if successful, False if not.
+        :rtype: bool
+    */
+    bool up(const size_t amount=5) {
+        std::stringstream stream;
+        stream << std::hex << amount;
+        std::string hex_string = stream.str();
+        if (hex_string.length() < 2) {
+            hex_string = "0" + hex_string;
+        }
+        std::string comm = "81010601VVWW0301FF";
+        comm.replace(8, 2, hex_string).replace(10, 2, "15");
+        ptContinuousMotion = true;
+        return command(comm);
+    }
+
+    /*
+    Modifies pan speed to down.
+
+        :param amount: Speed (0-24)
+        :return: True if successful, False if not.
+        :rtype: bool
+    */
+    bool down(const size_t amount=5) {
+        std::stringstream stream;
+        stream << std::hex << amount;
+        std::string hex_string = stream.str();
+        if (hex_string.length() < 2) {
+            hex_string = "0" + hex_string;
+        }
+        std::string comm = "81010601VVWW0302FF";
+        comm.replace(8, 2, hex_string).replace(10, 2, "15");
+        ptContinuousMotion = true;
+        return command(comm);
+    }
+
+    bool leftUp(const int pan, const int tilt) {
+        std::string comm = "81010601VVWW0101FF";
+        return move(comm, pan, tilt);
+    }
+
+    bool rightUp(const int pan, const int tilt) {
+        std::string comm = "81010601VVWW0201FF";
+        return move(comm, pan, tilt);
+    }
+
+    bool leftDown(const int pan, const int tilt) {
+        std::string comm = "81010601VVWW0102FF";
+        return move(comm, pan, tilt);
+    }
+
+    bool rightDown(const int pan, const int tilt) {
+        std::string comm = "81010601VVWW0202FF";
+        return move(comm, pan, tilt);
+    }
 };
 
 int main(int argc, char* argv[]) {
@@ -156,21 +442,11 @@ int main(int argc, char* argv[]) {
     const std::string HOSTNAME("192.168.1.40");
     UDPCamera myCam(HOSTNAME, PORT);
     myCam.connect();
-    //std::vector<uint8_t> comm({0x81, 0x01, 0x06, 0x04, 0xFF}); // Home position
-    //std::vector<uint8_t> comm({0x81, 0x09, 0x06, 0x23, 0xFF}); // Video format
-    //std::vector<uint8_t> comm({0x81, 0x09, 0x06, 0x12, 0xFF}); // Inquire position
-    std::vector<uint8_t> comm({0x81, 0x01, 0x06, 0x02, 0x18, 0x14, 0x0F, 0x0A, 0x0E, 0x01, 0x00, 0x03, 0x0E, 0x08, 0xFF});
-
+    // std::string comm = 81010604FF; // Home position
+    // std::string comm = 81090623FF; // Video format
+    // std::string comm = 81090612FF; // Inquire position
+    // std::string comm = 8101060218140F0A0E0100030E08FF; // absolute position
+    std::string comm = "81010604FF";
+    myCam.command(comm);
 
 }
-
-//
-// class PTZOptics20x {
-// private:
-//     _socket = None
-//     _host = None
-//     _tcp_port = None
-//
-// public:
-//     PTZOptics20x (arguments);
-// };
