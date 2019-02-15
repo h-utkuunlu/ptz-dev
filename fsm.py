5eee48ec007c79fb06431425eba6892bf60a203c
Daniel Chirita <dschirita@gmail.com>
2019-02-15 16:41:32 +0400

    converted tabs to spaces

--- fsm.py
+++ fsm.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-
+ 
 
 '''
 Finite state machine for ptz aerial tracking
@@ -41,57 +41,58 @@ from transitions import Machine, State
 from threading import Timer
 
 class Flow(object):
-	states=[
-		State('search'),
-		State('detect'),
-		State('id'),
-		State('track')
-	]
-	transitions = [
-	{ 'trigger': 'in_pos', 'source': 'search', 'dest': 'detect' },
-	{ 'trigger': 'timeout', 'source': 'detect', 'dest': 'search' },
-	{ 'trigger': 'found_obj', 'source': 'detect', 'dest': 'id' },
-	{ 'trigger': 'drone', 'source': 'id', 'dest': 'track' },
-	{ 'trigger': 'not_drone', 'source': 'id', 'dest': 'detect' },
-	{ 'trigger': 'lost_track', 'source': 'track', 'dest': 'search' }
-	]
-	
-	def __init__(self):
-		self.timer_expir = True # bool for if timer expired
-		self.obj_detected = False # bool for if object detected
-		self.machine = Machine(self, states=Flow.states, transitions=Flow.transitions, initial='search', auto_transitions=False)
-		#self.camera = 'source'
-		
-		
-	def expiry(self):
-		self.timer_expir = True
+    states=[
+        State('search'),
+        State('detect'),
+        State('id'),
+        State('track')
+    ]
+    transitions = [
+    { 'trigger': 'in_pos', 'source': 'search', 'dest': 'detect' },
+    { 'trigger': 'timeout', 'source': 'detect', 'dest': 'search' },
+    { 'trigger': 'found_obj', 'source': 'detect', 'dest': 'id' },
+    { 'trigger': 'drone', 'source': 'id', 'dest': 'track' },
+    { 'trigger': 'not_drone', 'source': 'id', 'dest': 'detect' },
+    { 'trigger': 'lost_track', 'source': 'track', 'dest': 'search' }
+    ]
+    
+    def __init__(self):
+        self.timer_expir = True # bool for if timer expired
+        self.obj_detected = False # bool for if object detected
+        self.machine = Machine(self, states=Flow.states, transitions=Flow.transitions, initial='search', auto_transitions=False)
+        #self.camera = 'source'
+        
+        
+    def expiry(self):
+        self.timer_expir = True
 
 ###########################################################
-		
-	def on_enter_search(self):
-		in_search_fn(self)		
-		
-	def on_exit_search(self):
-		out_search_fn(self)
-		
-	def on_enter_detect(self):
-		in_detect_fn(self)
+        
+    def on_enter_search(self):
+        in_search_fn(self)        
+        
+    def on_exit_search(self):
+        out_search_fn(self)
+        
+    def on_enter_detect(self):
+        in_detect_fn(self)
 
-	def on_exit_detect(self):
-		out_detect_fn(self)
+    def on_exit_detect(self):
+        out_detect_fn(self)
 
-	def on_enter_id(self):
-		in_id_fn(self)
-		
-	def on_exit_id(self):
-		out_id_fn(self)
+    def on_enter_id(self):
+        in_id_fn(self)
+        
+    def on_exit_id(self):
+        out_id_fn(self)
 
-	def on_enter_track(self):
-		in_track_fn(self)
-		
-	def on_exit_track(self):
-		out_track_fn(self)
-		
+    def on_enter_track(self):
+        in_track_fn(self)
+        
+    def on_exit_track(self):
+        out_track_fn(self)
+
+ 
 flow=Flow()
 
 flow.in_pos()
--- state_search.py
+++ state_search.py
@@ -1,11 +1,11 @@
 from threading import Timer
 import time
 def in_search_fn(parent):
-	print('searching')
-	time.sleep(1)
-	parent.in_pos()
+    print('searching')
+    time.sleep(1)
+    parent.in_pos()
 
 def out_search_fn(parent):
-	print('bg_initialized')
-	parent.timer_expir = False
-	Timer(5, parent.expiry, ()).start()
+    print('bg_initialized')
+    parent.timer_expir = False
+    Timer(5, parent.expiry, ()).start()
