3584a862dfa84cb6ffffd6df28164cc67f231cf8
Utku Unlu <h.utkuunlu@gmail.com>
2019-02-15 20:10:15 +0400

    Preliminary implementation of id state functions

--- state_id.py
+++ state_id.py
@@ -1,15 +1,25 @@
 import time
-import random
+import cv2
+import cv2.aruco as aruco
+
+aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_250)
+parameters = aruco.DetectorParameters_create()
 
 def in_id_fn(parent):
-    print('id-ing object')
-    time.sleep(1)
-    if random.random() > 0.5:
-        print('not drone :(')
-        parent.not_drone()
-    else:
-        print('drone !')
-        parent.drone()
+    print('=== id')
+
+    for i, img in enumerate(parent.cur_imgs):
+        _, ids, _ = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+
+        if ids and ids[0][0] == 42:
+            print("Found drone")
+            parent.drone_bbox = parent.cur_bbox[i]
+            parent.drone()
+        else:
+            print("Not a drone")
+    
+    print("No drone :(")
+    parent.not_drone()
 
 def out_id_fn(parent):
     pass
