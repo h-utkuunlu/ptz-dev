d0e037675898f76e4226565e6d07ac9c050c9a65
Utku Unlu <h.utkuunlu@gmail.com>
2019-02-15 16:42:29 +0400

    Convert tabs to spaces -> state_id.py and state_track.py

--- state_track.py
+++ state_track.py
@@ -1,9 +1,9 @@
 import time
 
 def in_track_fn(parent):
-	print('tracking')
-	time.sleep(5)
-	parent.lost_track()
+    print('tracking')
+    time.sleep(5)
+    parent.lost_track()
 
 def out_track_fn(parent):
-	pass
+    pass
