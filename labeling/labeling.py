d2eebfaacc83308243038fc2be5b29180e9691a7
Stefan <psn237@nyu.edu>
2019-02-24 11:19:56 +0400

    Automated labelling completed

--- labeling/labeling.py
+++ labeling/labeling.py
@@ -12,10 +12,13 @@ import numpy as np
 # as well as path for images and list of image names
 
 parser = argparse.ArgumentParser()
-parser.add_argument("folder", help="Folder name with images to make labels for")
-parser.add_argument("-o", "--output", default=None, help="Image label csv file name. Default is <folder>.csv")
+parser.add_argument('-f', '--folder', help="Folder name with images or video to make labels for")
+parser.add_argument('-v', '--video', default=None, help='Video stream name')
+parser.add_argument('-s', '--skip', default=1, help='Number of frames to skip when processing video', type=int)  # no specific reason for chosing 3
+parser.add_argument('-o', '--output', default=None, help="Image label csv file name. Default is <folder>.csv")
 args = parser.parse_args()
 
+# initialize variables for ease of processing
 refPt = []
 found_drone = False
 cropping = False
@@ -26,6 +29,11 @@ tracking = False
 tracker = cv2.TrackerCSRT_create()
 
 def click_and_crop(event, x, y, flags, param):
+    '''
+    Function called by OpenCV that visualizes and records the bounding box 
+    drawn by the user.
+    '''
+    
     # grab references to the global variables
     global refPt, cropping, cv_im, clone, tracker, tracking
     drawing = False
@@ -63,9 +71,19 @@ def click_and_crop(event, x, y, flags, param):
         cv2.imshow("image", cv_im)
         
 def format_x(x):
+    '''
+    Ensures that the number of columns for a BBOX is in a valid range 
+    of 0 to 1920 for the full HD image.
+    '''
+    
     return min(x, 1920) if (x > 0) else 0
     
 def format_y(y):
+    '''
+    Ensures that the number of rows for a BBOX is in a valid range of 0 to 1080
+    for the full HD image.
+    '''
+    
     return min(y, 1080) if (y > 0) else 0
 
 def auto_bbox():
@@ -74,7 +92,8 @@ def auto_bbox():
     Input: rgb image
     Output: Bounding box top left and bottom right coordinates ((x1, y1), (x2, y2))
     '''
-    global tracker, tracking, previous_image
+    
+    global tracker, tracking
     
     if (previous_image is None) or (not found_drone):
         return [(0, 0), (0, 0)]
@@ -95,14 +114,6 @@ def auto_bbox():
         tracker.clear()
         tracker = cv2.TrackerCSRT_create()
         return [(0, 0), (0, 0)] 
-        
-
-def print_help():
-    print("Usage:")
-    print("If the bounding box prediction is right, press 's' to save it")
-    print("If the estimate is wrong, draw the correct bounding box with the mouse.")
-    print("If the image does not belong to a drone, press 'd'")
-    print("Program terminates after cycling through all images in the 'images' folder")
 
 
 def add_label(outfile, im_name, prob, refPt):
@@ -118,6 +129,9 @@ def add_label(outfile, im_name, prob, refPt):
     return
 
 def format_xywh(refPt):
+    '''
+    Formats tuple refPT = ((x1, y1), (x2, y2)) into (x, y, w, h)
+    '''
     top_left_x  = format_x(min(refPt[0][0], refPt[1][0]))
     top_left_y = format_y(min(refPt[0][1], refPt[1][1]))
     bot_right_x = format_x(max(refPt[0][0], refPt[1][0]))
@@ -127,14 +141,11 @@ def format_xywh(refPt):
     h = abs(bot_right_y - top_left_y)  # absolute value not really necessary
     
     return top_left_x, top_left_y, w, h
-
-def unprocessed_images(outfile):
-    # Set file names to read from / save into
-    if args.output is not None:
-        outfile = args.output
-    else:
-        outfile = args.folder + ".csv"
-
+    
+def set_processed_images(outfile):
+    '''
+    Returns set of path names to all processed images of the form FOLDER/file.jpeg
+    '''
     # Check if any entries have been added before
     existing_entries = set()
     if os.path.isfile(outfile):
@@ -144,58 +155,115 @@ def unprocessed_images(outfile):
             if entry_list:
                 image_path = entry_list[0]
                 existing_entries.add(image_path)
-
-    # Populate the list of images to make a list for
-    images = []
-    total = 0
-    for file in os.listdir(args.folder):
-        if (file.endswith(".jpg") or file.endswith(".jpeg")) and (args.folder + "/" + file not in existing_entries):
-            images.append(args.folder + "/" + file)
-        total += 1
-    images.sort(key=extract_image_number)
-    return images, total
+        file.close()
+    return existing_entries
     
 def extract_image_number(file_name):
-    return int(file_name.split('(')[1].split(')')[0])
+    '''
+    Helper method for 'unprocessed_images' function by allowing sorting based on
+    the frame number, i.e., if file_name is of the form FOLDER_NAME/VIDEO_NAME(#).extension,
+    sorting would be based on #.
+    '''
+    
+    return int(file_name.split('(')[1].split(')')[0]) 
+    
+def print_help():
+    print("Usage:")
+    print("If the bounding box prediction is right, press 's' to save it")
+    print("If the estimate is wrong, draw the correct bounding box with the mouse.")
+    print("If the image does not belong to a drone, press 'd'")
+    print("Program terminates after cycling through all images in the 'images' folder")
+    
+def video_loader(video_path, existing_entries):
+    '''
+    Data generator for frames in video
+    '''
+    
+    # Number of previously processed entries
+    count_processed = len(existing_entries)
+    
+    # Open video
+    video_path_name = video_path.rsplit(".")[0]  # take only the video name without prefix
+    print(video_path)
+    video = cv2.VideoCapture(video_path)
+    count_total = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    success, frame = video.read()
+    count = 1
+    while success:
+        print("-----Loaded the image frame-----")
+        image_path = "{}({}).jpeg".format(video_path_name, count)  # the image path name if this frame were explicitely saved as a jpeg
+        if (image_path not in existing_entries) and (count % int(args.skip) == 0):
+            print("{} / {}".format(count, count_total))
+            yield frame, image_path
+        success, frame = video.read()
+        count += 1
+    video.release()    
+    
+def image_loader(folder, existing_entries):
+    '''
+    Data generator for images in folder
+    '''
+    
+    # Number of previously processed entries
+    count_processed = len(existing_entries)
+    
+    # Populate the list of images to make a list for
+    images = []
+    count_total = 0
+    for file in os.listdir(folder):
+        if (file.endswith(".jpg") or file.endswith(".jpeg")):
+            image = folder + "/" + file
+            images.append(image)
+        count_total += 1
+    images.sort(key=extract_image_number)
+    count = 1
+    for image_path in images:
+        if image_path not in existing_entries:
+            print("{} / {}".format(count, count_total))
+            image = cv2.imread(image_path)
+            yield image, image_path
+        count += 1
+    return images
     
 
 def main():
-    global refPt, cropping, cv_im, clone, previous_image, found_drone
-
+    global refPt, cv_im, clone, previous_image, found_drone
+    
+    # initialize output file name
     if args.output is not None:
         outfile = args.output
     else:
         outfile = args.folder + ".csv"
-
-    lst_unlabeled_images, total_num_images = unprocessed_images(outfile)
-    total_num_processed = total_num_images - len(lst_unlabeled_images)
-    # Check if the entire dataset have been processed before
-    if not lst_unlabeled_images:
-        print("All images in the given folder have been labeled. Exiting.. ")
-        exit()
+    
+    # create set of previously processed images / frames
+    existing_entries = set_processed_images(outfile)
+        
+    # setup iterator for image capture
+    if args.video is not None:  # video stream if using video option
+        print("Loading video")
+        video_path = args.folder + "/" + args.video
+        images = video_loader(video_path, existing_entries)
+        print("Successfully loaded video")
     else:
-        print_help()
+        print("Loading images from folder")
+        images = image_loader(args.folder, existing_entries)
+        print("Successfully loaded images form folder")
+        
+    print_help()
 
-    for image in lst_unlabeled_images:
+    for image, image_path in images:
+        
         if clone is not None:
             previous_image = clone.copy()
-        cv_im = cv2.imread(image)
+        cv_im = image
         clone = cv_im.copy()
         refPt = auto_bbox()
+        
         cv2.rectangle(cv_im, refPt[0], refPt[1], (0, 255, 0), 2)
-
         cv2.namedWindow("image", cv2.WINDOW_NORMAL)
         cv2.moveWindow("image", 1366, 0)
         cv2.setMouseCallback("image", click_and_crop)
-        cv2.putText(img=cv_im,
-                    text=str(total_num_processed) + ' / ' + str(total_num_images),
-                    org=(1200, 900),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                    fontScale=3,
-                    color=(0, 255, 0),
-                    thickness=2,
-                    lineType=cv2.LINE_AA)
-                    
+        
         # keep looping until the 'q' key is pressed
         cycle = True
         while cycle:
@@ -206,34 +274,33 @@ def main():
             
             # if the 'x' key is pressed, the image is not viable. Move image to deleted folder
             if key == ord("x"):
-                image_folder = image.split('/')[0]
+                image_folder = image_path.split('/')[0]
                 del_dir = "deleted/" + image_folder
                 if not os.path.isdir(del_dir):
                     if not os.path.isdir("deleted"):
                         os.mkdir("deleted")
                     os.mkdir(del_dir)
-                shutil.move(image, del_dir)
+                shutil.move(image_path, del_dir)
                 found_drone = False
                 break
 
             # if the 'd' key is pressed, there is no drone in the image
             elif key == ord("d"):
                 zeros = [(0, 0), (0, 0)]
-                add_label(outfile, image, "0", zeros)
+                add_label(outfile, image_path, "0", zeros)
                 found_drone = False
                 break
 
             # if the 's' key is pressed, save the bbox coordinates and break from the loop
             elif key == ord("s"): 
-                add_label(outfile, image, "1", refPt)
+                add_label(outfile, image_path, "1", refPt)
                 found_drone = True
                 break
 
             # if the 'q' key is pressed, stop labeling and exit
             elif key == ord("q"):
                 cycle = False
-        
-        total_num_processed += 1
+
         if not cycle:
             break
 
